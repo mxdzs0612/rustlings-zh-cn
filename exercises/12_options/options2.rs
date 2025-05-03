c76f755bb9e32dcc2750b31d58eee90780443fa7
mingye.jmy <mingye.jmy@alibaba-inc.com>
2025-05-03 22:07:34 +0800

    12 fin

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -10,9 +10,12 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: 将其转换为一个值为 `Some` 的 `if-let` 语句。
-        word = optional_target {
+        // word = optional_target {
+        //     assert_eq!(word, target);
+        // }
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
-        }
+        } 
     }
 
     #[test]
@@ -29,11 +32,14 @@ mod tests {
         // TODO: 将其转换为一个 `while-let` 语句。
         // 注意，`Vec::pop()` 会额外添加一层 `Option`。
         // 你可以在 `if-let` 和 `while-let` 语句中进行嵌套的模式匹配(nested pattern match)。
-        integer = optional_integers.pop() {
+        // integer = optional_integers.pop() {
+        //     assert_eq!(integer, cursor);
+        //     cursor -= 1;
+        // }
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
-
         assert_eq!(cursor, 0);
     }
 }
